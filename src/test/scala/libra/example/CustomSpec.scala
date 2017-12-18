0a80aff60369dde727122397c38019604c82b578
zainab-ali <zainab.ali.london@gmail.com>
2017-12-18 23:57:25 +0000

    Add shapeless witness

--- src/test/scala/libra/example/CustomSpec.scala
+++ src/test/scala/libra/example/CustomSpec.scala
@@ -2,9 +2,40 @@ package libra.example
 
 import org.scalatest._
 
+import libra._
+import libra.ops.base.Show
+import shapeless._
+import spire.algebra._
+import spire.implicits._
+import spire.math._
+
 class CustomSpec extends WordSpec with Matchers {
 
+  import CustomSpec._
+
   "libra" should {
-    "facilitate custom units using witnesses" in pending
+    "facilitate custom units using witnesses" in {
+      assert(3.gallons.show === "693 in^3 [L^3]")
+    }
+  }
+}
+
+object CustomSpec {
+
+  type CustomLength
+  implicit def lengthShow: Show[CustomLength] = Show[CustomLength]("L")
+
+  trait Inch extends UnitOfMeasure[CustomLength]
+
+  implicit def inchShow: Show[Inch] = Show[Inch]("in")
+
+  val wOne = Witness(1)
+  val wThree = Witness(3)
+
+  type InchesCubed[A] = Quantity[A, Term[CustomLength, Inch, Fraction[wThree.T, wOne.T]] :: HNil]
+
+  implicit final class QuantityGallonOps[A](a: A) {
+    def gallons(implicit M: MultiplicativeSemigroup[A], C: ConvertableTo[A]): InchesCubed[A] =
+      Quantity(a * C.fromInt(231))
   }
 }
