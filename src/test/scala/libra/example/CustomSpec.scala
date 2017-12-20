e5ae0bbf421da579e407ef8eb524c56b97e80cf6
zainab-ali <zainab.ali.london@gmail.com>
2017-12-20 00:24:46 +0000

    Add roman units to custom spec

--- src/test/scala/libra/example/CustomSpec.scala
+++ src/test/scala/libra/example/CustomSpec.scala
@@ -11,31 +11,49 @@ import spire.math._
 
 class CustomSpec extends WordSpec with Matchers {
 
-  import CustomSpec._
+  import RomanLengths._
 
   "libra" should {
-    "facilitate custom units using witnesses" in {
-      assert(3.gallons.show === "693 in^3 [L^3]")
+    "show custom units" in {
+      assert(42.pes3.show === "42 pes^3 [R^3]")
+    }
+
+    "divide custom units" in {
+      assert(10.pes3 /~ 5.pes2 === 2.pes)
     }
   }
 }
 
-object CustomSpec {
+object RomanLengths {
 
-  type CustomLength
-  implicit def lengthShow: Show[CustomLength] = Show[CustomLength]("L")
+  type RomanLength
 
-  trait Inch extends UnitOfMeasure[CustomLength]
+  implicit def romanLengthShow: Show[RomanLength] = Show[RomanLength]("R")
 
-  implicit def inchShow: Show[Inch] = Show[Inch]("in")
+  /** Roman foot */
+  trait Pes extends UnitOfMeasure[RomanLength]
+
+  /** Roman palm */
+  trait Palmus extends UnitOfMeasure[RomanLength]
+
+  implicit def pesShow: Show[Pes] = Show[Pes]("pes")
+  implicit def palmusShow: Show[Palmus] = Show[Palmus]("palmus")
 
   val wOne = Witness(1)
+  val wTwo = Witness(2)
   val wThree = Witness(3)
 
-  type InchesCubed[A] = Quantity[A, Term[CustomLength, Inch, Fraction[wThree.T, wOne.T]] :: HNil]
+  type LengthSquared[A, L <: UnitOfMeasure[RomanLength]] = Quantity[A, Term[RomanLength, L, Fraction[wTwo.T, wOne.T]] :: HNil]
+  type LengthCubed[A, L <: UnitOfMeasure[RomanLength]] = Quantity[A, Term[RomanLength, L, Fraction[wThree.T, wOne.T]] :: HNil]
 
-  implicit final class QuantityGallonOps[A](a: A) {
-    def gallons(implicit M: MultiplicativeSemigroup[A], C: ConvertableTo[A]): InchesCubed[A] =
-      Quantity(a * C.fromInt(231))
+  implicit final class RomanQuantityOps[A](a: A) {
+    def pes: QuantityOf[A, RomanLength, Pes] = Quantity(a)
+    def palmus: QuantityOf[A, RomanLength, Palmus] = Quantity(a)
+
+    def pes2: LengthSquared[A, Pes] = Quantity(a)
+    def palmus2: LengthSquared[A, Palmus] = Quantity(a)
+
+    def pes3: LengthCubed[A, Pes] = Quantity(a)
+    def palmus3: LengthSquared[A, Palmus] = Quantity(a)
   }
 }
